bc93d1f06af923e63bd586b3f8ef1f76de13bcb7
Paloma Rehecho <paloma_re@hotmail.com>
2024-06-19 15:50:07 +0100

    welcome page finished

--- Pages/ProjectsPage/ProjectsPage.jsx
+++ Pages/ProjectsPage/ProjectsPage.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Container, Row, Col, Card, Button } from "react-bootstrap"
+import { Row, Col, Card, Button } from "react-bootstrap"
 import './ProjectsPage.css'
 import { Link } from "react-router-dom"
 
@@ -8,7 +8,7 @@ const ProjectsPage = () => {
 
 
     return (
-        <Row className="ProjectsPage" style={{ margin: 0, padding: 50 }}>
+        <Row className="ProjectsPage" style={{ margin: 0, padding: 20 }}>
             <Col xs={12} md={6} lg={4}>
                 <Card>
                     <Link to={'https://wavescope.netlify.app/'}>
@@ -26,7 +26,9 @@ const ProjectsPage = () => {
                         <Link to={'https://github.com/Drialis/Wave-Scope-client'}>
                             <Button className="projectButton">Client Repository</Button>
                         </Link>
-                        <Button className="projectButton">Server Repository</Button>
+                        <Link to={'https://github.com/Drialis/Wave-Scope-API'}>
+                            <Button className="projectButton">Server Repository</Button>
+                        </Link>
                     </Card.Body>
                 </Card>
             </Col>
@@ -35,14 +37,20 @@ const ProjectsPage = () => {
                 <Card>
                     <Card.Img variant="top" src="https://res.cloudinary.com/du8mlfcf9/image/upload/v1718799233/Captura_de_pantalla_2024-06-08_101800_jb3tdq.png" />
                     <Card.Body>
-                        <Card.Title>Wave Scope</Card.Title>
+                        <Card.Title>Hack a Jet</Card.Title>
                         <Card.Text>
                             Some quick example text to build on the card title and make up the
                             bulk of the card's content.
                         </Card.Text>
-                        <Button className="projectButton">Go to Hack a Jet Web</Button>
-                        <Button className="projectButton">Client Repository</Button>
-                        <Button className="projectButton">Server Repository</Button>
+                        <Link to={'https://hackajet.netlify.app/'}>
+                            <Button className="projectButton">Go to Hack a Jet Web</Button>
+                        </Link>
+                        <Link to={'https://github.com/Drialis/Hack-a-Jet-client'}>
+                            <Button className="projectButton">Client Repository</Button>
+                        </Link>
+                        <Link to={'https://github.com/Drialis/Hack-a-Jet-server'}>
+                            <Button className="projectButton">Server Repository</Button>
+                        </Link>
                     </Card.Body>
                 </Card>
             </Col>
@@ -51,14 +59,17 @@ const ProjectsPage = () => {
                 <Card>
                     <Card.Img variant="top" src="https://res.cloudinary.com/du8mlfcf9/image/upload/v1718799457/Captura_de_pantalla_2024-06-19_131721_cafxie.png" />
                     <Card.Body>
-                        <Card.Title>Wave Scope</Card.Title>
+                        <Card.Title>Julita's Dungeon Game</Card.Title>
                         <Card.Text>
                             Some quick example text to build on the card title and make up the
                             bulk of the card's content.
                         </Card.Text>
-                        <Button className="projectButton">Go to Julitas Dungeon</Button>
-                        <Button className="projectButton">Client Repository</Button>
-                        <Button className="projectButton">Server Repository</Button>
+                        <Link to={'https://drialis.github.io/julitas-dungeon-game/'}>
+                            <Button className="projectButton">Go to Julitas Dungeon</Button>
+                        </Link>
+                        <Link to={'https://github.com/Drialis/julitas-dungeon-game'}>
+                            <Button className="projectButton">Full Repository</Button>
+                        </Link>
                     </Card.Body>
                 </Card>
             </Col>
--- Pages/WelcomePage/WelcomePage.jsx
+++ Pages/WelcomePage/WelcomePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Container, Row, Col } from 'react-bootstrap';
+import { Card, Container, Row, Col, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './WelcomePage.css';
 
@@ -7,7 +7,7 @@ const WelcomePage = () => {
     return (
         <Container fluid className='WelcomePage g-0'>
             <Row>
-                <Col xs={12} md={6} lg={6} style={{ padding: 0 }}>
+                <Col xs={12} md={6} lg={5} style={{ padding: 0 }}>
                     <Card className='br-0'>
                         <Card.Img
                             className="Paloma Rehecho Antolín"
@@ -16,17 +16,29 @@ const WelcomePage = () => {
                     </Card>
                 </Col>
 
-                <Col xs={12} md={6} lg={6}>
-                    <Card>
+                <Col xs={12} md={6} lg={7}>
+                    <Card className='WelcomeText'>
                         <Card.Body style={{ height: '400px' }}>
+                            <Card.Img variant='top' src='https://res.cloudinary.com/du8mlfcf9/image/upload/v1718808024/Presentaci%C3%B3n_diapositiva_Marca_creativa_Doodle_Blanco_y_marr%C3%B3n_gsapy7.png'></Card.Img>
                             <Card.Title>Full Stack Developer</Card.Title>
                             <br />
                             <Card.Text className="fw-lighter">
-                                This full stack developer is a dreamer who gets lost in scientific and fantasy books. She invests her time learning new technologies and practicing her favorite sport, taekwondo.
+                                My development in the IT world continues evolving. After completing several courses on my own,
+                                I decided to further my studies by attending the Web Developer bootcamp at Ironhack.
+                                With the valuable skills gained during this training,
+                                I'm actively seeking opportunities to join the industry, as you can see in my repositories.
                             </Card.Text>
+
                             <br />
-                            <Link className="GitHubLink fw-bold" to="https://github.com/Drialis">
-                                🔗 GitHub Link
+                            <Link to="https://github.com/Drialis">
+                                <Button className="GitHubLink">
+                                    🔗 GitHub Link
+                                </Button>
+                            </Link>
+                            <Link>
+                                <Button className='AboutMe'>
+                                    ▷ About me
+                                </Button>
                             </Link>
                         </Card.Body>
                     </Card>
