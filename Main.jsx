0c26e381e4ba72996a01663858d3713c948e0b6b
Paloma Rehecho <paloma_re@hotmail.com>
2024-06-21 14:38:15 +0100

    testing react

--- Main.jsx
+++ Main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
     <Router>
         <React.StrictMode>
             <App />
