d73ff3a7b4908348491cbd36252246b6f0732a1c
Paloma Rehecho <paloma_re@hotmail.com>
2024-09-05 15:06:29 +0100

    starting translations

--- App/App.jsx
+++ App/App.jsx
@@ -1,4 +1,4 @@
-import AppRoutes from './routes/AppRoutes';
+import AppRoutes from '../routes/AppRoutes';
 import Navigation from './Components/Navigation/Navigation';
 import Footer from './Components/Footer/Footer';
 import React from 'react';
