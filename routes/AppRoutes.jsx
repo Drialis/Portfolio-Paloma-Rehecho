361791c77b237d366890eb0dc6a61fcb9b2966a5
Paloma Rehecho <paloma_re@hotmail.com>
2024-09-10 12:43:40 +0100

    translation logics done

--- routes/AppRoutes.jsx
+++ routes/AppRoutes.jsx
@@ -17,21 +17,15 @@ const AppRoutes = () => {
     return (
         <Container fluid className="AppRoutes">
             <Routes>
-                {/* <Route path="/" element={<Navigate to={`/${userLang}`}/>} /> */}
 
-                {/* <Route path="/:lng(en|es)" element={<WelcomePage />} /> */}
                 <Route path="/" element={<WelcomePage />} />
-                {/* <Route path="/:lng(en|es)/projects" element={<ProjectsPage />} /> */}
                 <Route path="/projects" element={<ProjectsPage />} />
-                {/* <Route path="/:lng(en|es)/it-tools" element={<ItToolsPage />} /> */}
                 <Route path="/it-tools" element={<ItToolsPage />} />
-                {/* <Route path="/:lng(en|es)/about-me" element={<AboutMe />} /> */}
                 <Route path="/about-me" element={<AboutMe />} />
-                {/* <Route path="/:lng(en|es)/contact" element={<ContactPage />} /> */}
-                <Route path="/contact" element={<ContactPage />} />                
+                <Route path="/contact" element={<ContactPage />} />     
+                 {/*TODO: */}           
                 {/* <Route path="/custom-map" element={<CustomMap />} /> */}
                 {/* <Route path="/*" element={<NotFoundPage />} /> */}
-                {/* <Route path="*" element={<Navigate to={`/${userLang}`}/>} /> */}
 
             </Routes>
         </Container>
--- src/Localization/locales/i18n.js
+++ src/Localization/locales/i18n.js
@@ -15,42 +15,22 @@ export const resources = {
 
 export const defaultNS = "translation";
 
-const languageDetector = {
-  type: "languageDetector",
-  detect: () => {
-    const locale = navigator.language || navigator.userLanguage;
-    const lang = locale.split("-")[0];
-    const supportedLocales = ["es", "en"];
-    return supportedLocales.includes(lang) ? lang : "en";
-  },
-  init: () => {},
-  cacheUserLanguage: () => {}
-};
-
 i18n
- // .use(languageDetector)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources,
-    en:{
-      translation: en
-    },
-    es: {
-      translation: es
-    },
-    detection: {
-      order: ['localStorage', 'cookie', 'navigator', 'htmlTag', 'path', 'subdomain'],
-      caches: ['localStorage', 'cookie']
-    },
-    lng: 'en', 
     fallbackLng: "en",
+    debug: true,
     interpolation: {
       escapeValue: false,
     },
+    detection: {
+      order: ['localStorage', 'cookie', 'navigator', 'htmlTag'],
+      caches: ['localStorage', 'cookie']
+    },
     ns: [defaultNS],
     defaultNS,
-    debug: true,
   });
 
 export default i18n;
